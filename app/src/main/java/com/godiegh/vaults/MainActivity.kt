3f9c08de361229442fab07d3f6acd199be7e5f93
godieGH <godiejoseph20@gmail.com>
2026-07-05 01:53:34 +0300

    Fix build bugs are some ffi connection

--- app/src/main/java/com/godiegh/vaults/MainActivity.kt
+++ app/src/main/java/com/godiegh/vaults/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import com.godiegh.vaults.ui.theme.VaultsTheme
+import uniffi.vaults.ffiGenerateSalt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +20,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             VaultsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    // starting to write the app here
-                    Text(text = "Hello World", modifier = Modifier
-                        .padding(innerPadding)
+                    val salt = ffiGenerateSalt()
+                    Text(
+                        text = "Salt: ${salt.take(8).joinToString("") { "%02x".format(it) }}...",
+                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
